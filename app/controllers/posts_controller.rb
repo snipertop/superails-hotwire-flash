0626f92259e937ee929eb04089a27095b40b6cf5
dinian <1361149082@qq.com>
2022-04-10 18:05:27 +0800

    fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,9 +25,21 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
+        flash.now[:notice] = "#{@post.id} added at #{Time.zone.now}"
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update("new_post", partial: "posts/form", locals: { post: Post.new }),
+            turbo_stream.prepend("posts", partial: "posts/post", locals: { post: @post }),
+            turbo_stream.prepend("flash", partial: "layouts/flash"),
+
+          ]
+        end
         format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.update("new_post", partial: "posts/form", locals: { post: @post } )
+        end
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
@@ -52,6 +64,9 @@ class PostsController < ApplicationController
     @post.destroy
 
     respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.update("posts", partial: "posts/post", locals: { post: @post } )
+      end
       format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
       format.json { head :no_content }
     end
